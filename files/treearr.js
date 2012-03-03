478c4eecec0464a89f500f4810b1360e11137cd1
Zero <zero@h-sec.org>
2012-03-03 17:42:13 +0100

    Primer commit - Original

---
+++ files/treearr.js
@@ -0,0 +1,239 @@
+var TITEMS = [ 
+ ["C++ GUI Programming with Qt 4, Second Edition - Graphically Rich Book", "sources/final/main.html", "11"],
+ ["Table of Contents", "sources/final/toc.html", "21"],
+ ["Copyright", "sources/final/copyrightpg.html", "17"],
+ ["Series Editor's Note", "sources/final/pref01.html", "1"],
+ ["Foreword", "sources/final/pref02.html", "1"],
+ ["Preface", "sources/final/pref03.html", "1"],
+ ["Acknowledgments", "sources/final/pref04.html", "1"],
+ ["A Brief History of Qt", "sources/final/pref05.html", "1"],
+ ["Part I: Basic Qt", "sources/final/part01.html", "1",
+  ["Chapter 1. Getting Started", "sources/final/ch01.html", "1",
+   ["Hello Qt", "sources/final/ch01lev1sec1.html", "11"],
+   ["Making Connections", "sources/final/ch01lev1sec2.html", "11"],
+   ["Laying Out Widgets", "sources/final/ch01lev1sec3.html", "11"],
+   ["Using the Reference Documentation", "sources/final/ch01lev1sec4.html", "11"]
+  ],
+  ["Chapter 2. Creating Dialogs", "sources/final/ch02.html", "1",
+   ["Subclassing QDialog", "sources/final/ch02lev1sec1.html", "11"],
+   ["Signals and Slots in Depth", "sources/final/ch02lev1sec2.html", "11"],
+   ["Rapid Dialog Design", "sources/final/ch02lev1sec3.html", "11"],
+   ["Shape-Changing Dialogs", "sources/final/ch02lev1sec4.html", "11"],
+   ["Dynamic Dialogs", "sources/final/ch02lev1sec5.html", "11"],
+   ["Built-in Widget and Dialog Classes", "sources/final/ch02lev1sec6.html", "11"]
+  ],
+  ["Chapter 3. Creating Main Windows", "sources/final/ch03.html", "1",
+   ["Subclassing QMainWindow", "sources/final/ch03lev1sec1.html", "11"],
+   ["Creating Menus and Toolbars", "sources/final/ch03lev1sec2.html", "11"],
+   ["Setting Up the Status Bar", "sources/final/ch03lev1sec3.html", "11"],
+   ["Implementing the File Menu", "sources/final/ch03lev1sec4.html", "11"],
+   ["Using Dialogs", "sources/final/ch03lev1sec5.html", "11"],
+   ["Storing Settings", "sources/final/ch03lev1sec6.html", "11"],
+   ["Multiple Documents", "sources/final/ch03lev1sec7.html", "11"],
+   ["Splash Screens", "sources/final/ch03lev1sec8.html", "11"]
+  ],
+  ["Chapter 4. Implementing Application Functionality", "sources/final/ch04.html", "1",
+   ["The Central Widget", "sources/final/ch04lev1sec1.html", "11"],
+   ["Subclassing QTableWidget", "sources/final/ch04lev1sec2.html", "11"],
+   ["Loading and Saving", "sources/final/ch04lev1sec3.html", "11"],
+   ["Implementing the Edit Menu", "sources/final/ch04lev1sec4.html", "11"],
+   ["Implementing the Other Menus", "sources/final/ch04lev1sec5.html", "11"],
+   ["Subclassing QTableWidgetItem", "sources/final/ch04lev1sec6.html", "11"]
+  ],
+  ["Chapter 5. Creating Custom Widgets", "sources/final/ch05.html", "1",
+   ["Customizing Qt Widgets", "sources/final/ch05lev1sec1.html", "11"],
+   ["Subclassing QWidget", "sources/final/ch05lev1sec2.html", "11"],
+   ["Integrating Custom Widgets with Qt Designer", "sources/final/ch05lev1sec3.html", "11"],
+   ["Double Buffering", "sources/final/ch05lev1sec4.html", "11"]
+  ]
+ ],
+ ["Part II: Intermediate Qt", "sources/final/part02.html", "1",
+  ["Chapter 6. Layout Management", "sources/final/ch06.html", "1",
+   ["Laying Out Widgets on a Form", "sources/final/ch06lev1sec1.html", "11"],
+   ["Stacked Layouts", "sources/final/ch06lev1sec2.html", "11"],
+   ["Splitters", "sources/final/ch06lev1sec3.html", "11"],
+   ["Scrolling Areas", "sources/final/ch06lev1sec4.html", "11"],
+   ["Dock Windows and Toolbars", "sources/final/ch06lev1sec5.html", "11"],
+   ["Multiple Document Interface", "sources/final/ch06lev1sec6.html", "11"]
+  ],
+  ["Chapter 7. Event Processing", "sources/final/ch07.html", "1",
+   ["Reimplementing Event Handlers", "sources/final/ch07lev1sec1.html", "11"],
+   ["Installing Event Filters", "sources/final/ch07lev1sec2.html", "11"],
+   ["Staying Responsive during Intensive Processing", "sources/final/ch07lev1sec3.html", "11"]
+  ],
+  ["Chapter 8. 2D Graphics", "sources/final/ch08.html", "1",
+   ["Painting with QPainter", "sources/final/ch08lev1sec1.html", "11"],
+   ["Coordinate System Transformations", "sources/final/ch08lev1sec2.html", "11"],
+   ["High-Quality Rendering with QImage", "sources/final/ch08lev1sec3.html", "11"],
+   ["Item-Based Rendering with Graphics View", "sources/final/ch08lev1sec4.html", "11"],
+   ["Printing", "sources/final/ch08lev1sec5.html", "11"]
+  ],
+  ["Chapter 9. Drag and Drop", "sources/final/ch09.html", "1",
+   ["Enabling Drag and Drop", "sources/final/ch09lev1sec1.html", "11"],
+   ["Supporting Custom Drag Types", "sources/final/ch09lev1sec2.html", "11"],
+   ["Clipboard Handling", "sources/final/ch09lev1sec3.html", "11"]
+  ],
+  ["Chapter 10. Item View Classes", "sources/final/ch10.html", "1",
+   ["Using the Item View Convenience Classes", "sources/final/ch10lev1sec1.html", "11"],
+   ["Using Predefined Models", "sources/final/ch10lev1sec2.html", "11"],
+   ["Implementing Custom Models", "sources/final/ch10lev1sec3.html", "11"],
+   ["Implementing Custom Delegates", "sources/final/ch10lev1sec4.html", "11"]
+  ],
+  ["Chapter 11. Container Classes", "sources/final/ch11.html", "1",
+   ["Sequential Containers", "sources/final/ch11lev1sec1.html", "11"],
+   ["Associative Containers", "sources/final/ch11lev1sec2.html", "11"],
+   ["Generic Algorithms", "sources/final/ch11lev1sec3.html", "11"],
+   ["Strings, Byte Arrays, and Variants", "sources/final/ch11lev1sec4.html", "11"]
+  ],
+  ["Chapter 12. Input/Output", "sources/final/ch12.html", "1",
+   ["Reading and Writing Binary Data", "sources/final/ch12lev1sec1.html", "11"],
+   ["Reading and Writing Text", "sources/final/ch12lev1sec2.html", "11"],
+   ["Traversing Directories", "sources/final/ch12lev1sec3.html", "11"],
+   ["Embedding Resources", "sources/final/ch12lev1sec4.html", "11"],
+   ["Inter-Process Communication", "sources/final/ch12lev1sec5.html", "11"]
+  ],
+  ["Chapter 13. Databases", "sources/final/ch13.html", "1",
+   ["Connecting and Querying", "sources/final/ch13lev1sec1.html", "11"],
+   ["Viewing Tables", "sources/final/ch13lev1sec2.html", "11"],
+   ["Editing Records Using Forms", "sources/final/ch13lev1sec3.html", "11"],
+   ["Presenting Data in Tabular Forms", "sources/final/ch13lev1sec4.html", "11"]
+  ],
+  ["Chapter 14. Multithreading", "sources/final/ch14.html", "1",
+   ["Creating Threads", "sources/final/ch14lev1sec1.html", "11"],
+   ["Synchronizing Threads", "sources/final/ch14lev1sec2.html", "11"],
+   ["Communicating with the Main Thread", "sources/final/ch14lev1sec3.html", "11"],
+   ["Using Qt's Classes in Secondary Threads", "sources/final/ch14lev1sec4.html", "11"]
+  ],
+  ["Chapter 15. Networking", "sources/final/ch15.html", "1",
+   ["Writing FTP Clients", "sources/final/ch15lev1sec1.html", "11"],
+   ["Writing HTTP Clients", "sources/final/ch15lev1sec2.html", "11"],
+   ["Writing TCP Client-Server Applications", "sources/final/ch15lev1sec3.html", "11"],
+   ["Sending and Receiving UDP Datagrams", "sources/final/ch15lev1sec4.html", "11"]
+  ],
+  ["Chapter 16. XML", "sources/final/ch16.html", "1",
+   ["Reading XML with QXmlStreamReader", "sources/final/ch16lev1sec1.html", "11"],
+   ["Reading XML with DOM", "sources/final/ch16lev1sec2.html", "11"],
+   ["Reading XML with SAX", "sources/final/ch16lev1sec3.html", "11"],
+   ["Writing XML", "sources/final/ch16lev1sec4.html", "11"]
+  ],
+  ["Chapter 17. Providing Online Help", "sources/final/ch17.html", "1",
+   ["Tooltips, Status Tips, and &quot;What's This?&quot; Help", "sources/final/ch17lev1sec1.html", "11"],
+   ["Using a Web Browser to Provide Online Help", "sources/final/ch17lev1sec2.html", "11"],
+   ["Using QTextBrowser as a Simple Help Engine", "sources/final/ch17lev1sec3.html", "11"],
+   ["Using Qt Assistant for Powerful Online Help", "sources/final/ch17lev1sec4.html", "11"]
+  ]
+ ],
+ ["Part III: Advanced Qt", "sources/final/part03.html", "1",
+  ["Chapter 18. Internationalization", "sources/final/ch18.html", "1",
+   ["Working with Unicode", "sources/final/ch18lev1sec1.html", "11"],
+   ["Making Applications Translation-Aware", "sources/final/ch18lev1sec2.html", "11"],
+   ["Dynamic Language Switching", "sources/final/ch18lev1sec3.html", "11"],
+   ["Translating Applications", "sources/final/ch18lev1sec4.html", "11"]
+  ],
+  ["Chapter 19. Look and Feel Customization", "sources/final/ch19.html", "1",
+   ["Using Qt Style Sheets", "sources/final/ch19lev1sec1.html", "11"],
+   ["Subclassing QStyle", "sources/final/ch19lev1sec2.html", "11"]
+  ],
+  ["Chapter 20. 3D Graphics", "sources/final/ch20.html", "1",
+   ["Drawing Using OpenGL", "sources/final/ch20lev1sec1.html", "11"],
+   ["Combining OpenGL and QPainter", "sources/final/ch20lev1sec2.html", "11"],
+   ["Doing Overlays Using Framebuffer Objects", "sources/final/ch20lev1sec3.html", "11"]
+  ],
+  ["Chapter 21. Creating Plugins", "sources/final/ch21.html", "1",
+   ["Extending Qt with Plugins", "sources/final/ch21lev1sec1.html", "11"],
+   ["Making Applications Plugin-Aware", "sources/final/ch21lev1sec2.html", "11"],
+   ["Writing Application Plugins", "sources/final/ch21lev1sec3.html", "11"]
+  ],
+  ["Chapter 22. Application Scripting", "sources/final/ch22.html", "1",
+   ["Overview of the ECMAScript Language", "sources/final/ch22lev1sec1.html", "11"],
+   ["Extending Qt Applications with Scripts", "sources/final/ch22lev1sec2.html", "11"],
+   ["Implementing GUI Extensions Using Scripts", "sources/final/ch22lev1sec3.html", "11"],
+   ["Automating Tasks through Scripting", "sources/final/ch22lev1sec4.html", "11"]
+  ],
+  ["Chapter 23. Platform-Specific Features", "sources/final/ch23.html", "1",
+   ["Interfacing with Native APIs", "sources/final/ch23lev1sec1.html", "11"],
+   ["Using ActiveX on Windows", "sources/final/ch23lev1sec2.html", "11"],
+   ["Handling X11 Session Management", "sources/final/ch23lev1sec3.html", "11"]
+  ],
+  ["Chapter 24. Embedded Programming", "sources/final/ch24.html", "1",
+   ["Getting Started with Qt/Embedded Linux", "sources/final/ch24lev1sec1.html", "11"],
+   ["Customizing Qt/Embedded Linux", "sources/final/ch24lev1sec2.html", "11"],
+   ["Integrating Qt Applications with Qtopia", "sources/final/ch24lev1sec3.html", "11"],
+   ["Using Qtopia APIs", "sources/final/ch24lev1sec4.html", "11"]
+  ]
+ ],
+ ["Part IV: Appendixes", "sources/final/part04.html", "1",
+  ["Appendix A. Obtaining and Installing Qt", "sources/final/app01.html", "1",
+   ["A Note on Licensing", "sources/final/app01lev1sec1.html", "11"],
+   ["Installing Qt/Windows", "sources/final/app01lev1sec2.html", "11"],
+   ["Installing Qt/Mac", "sources/final/app01lev1sec3.html", "11"],
+   ["Installing Qt/X11", "sources/final/app01lev1sec4.html", "11"]
+  ],
+  ["Appendix B. Building Qt Applications", "sources/final/app02.html", "1",
+   ["Using qmake", "sources/final/app02lev1sec1.html", "11"],
+   ["Using Third-Party Build Tools", "sources/final/app02lev1sec2.html", "11"]
+  ],
+  ["Appendix C. Introduction to Qt Jambi", "sources/final/app03.html", "1",
+   ["Getting Started with Qt Jambi", "sources/final/app03lev1sec1.html", "11"],
+   ["Using Qt Jambi in the Eclipse IDE", "sources/final/app03lev1sec2.html", "11"],
+   ["Integrating C++ Components with Qt Jambi", "sources/final/app03lev1sec3.html", "11"]
+  ],
+  ["Appendix D. Introduction to C++ for Java and C# Programmers", "sources/final/app04.html", "1",
+   ["Getting Started with C++", "sources/final/app04lev1sec1.html", "11"],
+   ["Main Language Differences", "sources/final/app04lev1sec2.html", "11"],
+   ["The Standard C++ Library", "sources/final/app04lev1sec3.html", "11"]
+  ],
+  ["About the Authors", "sources/final/app05.html", "1",
+   ["Jasmin Blanchette", "sources/final/app05lev1sec1.html", "11"],
+   ["Mark Summerfield", "sources/final/app05lev1sec2.html", "11"]
+  ],
+  ["Production", "sources/final/app06.html", "1"]
+ ],
+ ["Index", "sources/final/index.html", "1",
+  ["SYMBOL", "sources/final/SYMBOL.html", "11"],
+  ["A", "sources/final/A.html", "11"],
+  ["B", "sources/final/B.html", "11"],
+  ["C", "sources/final/C.html", "11"],
+  ["D", "sources/final/D.html", "11"],
+  ["E", "sources/final/E.html", "11"],
+  ["F", "sources/final/F.html", "11"],
+  ["G", "sources/final/G.html", "11"],
+  ["H", "sources/final/H.html", "11"],
+  ["I", "sources/final/I.html", "11"],
+  ["J", "sources/final/J.html", "11"],
+  ["K", "sources/final/K.html", "11"],
+  ["L", "sources/final/L.html", "11"],
+  ["M", "sources/final/M.html", "11"],
+  ["N", "sources/final/N.html", "11"],
+  ["O", "sources/final/O.html", "11"],
+  ["P", "sources/final/P.html", "11"],
+  ["Q", "sources/final/Q.html", "11"],
+  ["R", "sources/final/R.html", "11"],
+  ["S", "sources/final/S.html", "11"],
+  ["T", "sources/final/T.html", "11"],
+  ["U", "sources/final/U.html", "11"],
+  ["V", "sources/final/V.html", "11"],
+  ["W", "sources/final/W.html", "11"],
+  ["X", "sources/final/X.html", "11"],
+  ["Z", "sources/final/Z.html", "11"]
+ ]
+];
+
+
+var FITEMS = arr_flatten(TITEMS);
+
+function arr_flatten (x) {
+   var y = []; if (x == null) return y;
+   for (var i=0; i<x.length; i++) {
+      if (typeof(x[i]) == "object") {
+         var flat = arr_flatten(x[i]);
+         for (var j=0; j<flat.length; j++)
+             y[y.length]=flat[j];
+      } else {
+         if ((i%3==0))
+          y[y.length]=x[i+1];
+      }
+   }
+   return y;
+}
+
